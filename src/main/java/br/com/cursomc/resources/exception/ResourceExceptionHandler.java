4a3786fdb117d46cf47936d4ef64a65e0de07b03
jeanbahia <jeanhbahia@gmail.com>
2018-04-29 18:07:19 -0300

    Ajustes no endpoint /categorias/id

---
+++ src/main/java/br/com/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package br.com.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.cursomc.resources.StandardError;
+import br.com.cursomc.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+	}
+
+}
--- src/main/java/br/com/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.cursomc.domain.Categoria;
 import br.com.cursomc.repositories.CategoriaRepository;
+import br.com.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,10 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
-	public Categoria buscar(Integer id) {
-		
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
+
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		
-		return categoria.orElse(null);
+
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrador! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
