d6d3292bce2e97aa1fcea905cea06d001c99db54
jeanbahia <jeanhbahia@gmail.com>
2018-04-29 18:13:12 -0300

    Ajustes nos pacotes dos filtros de erro

--- src/main/java/br/com/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import br.com.cursomc.resources.StandardError;
 import br.com.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
